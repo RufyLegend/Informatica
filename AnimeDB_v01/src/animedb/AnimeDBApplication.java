04280c0dcb04550cfd1864d9bb2de1fb4308148c
RufyLegend <nichk.ot@gmail.com>
2017-02-24 18:37:35 +0100

    Prima versione della grafica

---
+++ AnimeDB_v01/src/animedb/AnimeDBApplication.java
@@ -0,0 +1,15 @@
+package animedb;
+
+/**
+ *
+ * @author Nicholas Ottaviani
+ * @date 24 feb 2017 17:28:19
+ */
+public class AnimeDBApplication {
+	
+	public static void main(String args[]){
+		
+		GUI gui = new GUI();
+		gui.frame.setVisible(true);
+	}
+}
