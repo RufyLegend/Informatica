96ccb6542a86109c7e0d045c9e16b9a542ef2672
RufyLegend <nichk.ot@gmail.com>
2017-04-06 18:11:10 +0200

    File conf finito

---
+++ Logstash_App/src/logstash_app/LogstashProgram.java
@@ -0,0 +1,12 @@
+package logstash_app;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Nicholas Ottaviani
+ * @date 03 apr 2017 15:38:04
+ */
+public class LogstashProgram {
+}
