9872599600629e1104d14a1d1ced3b765e7fe4a5
RufyLegend <nichk.ot@gmail.com>
2017-05-05 15:10:41 +0200

    prova del commit

--- 13_2q_OttavianiNicholas_Films/inserisci.php
+++ 13_2q_OttavianiNicholas_Films/inserisci.php
@@ -3,13 +3,14 @@
 
 <!-- Intestazione -->
 <head>
-  <title>Inserisci</title>
-  <link rel="stylesheet" href="stile.css" type="text/css">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 
 
@@ -58,7 +59,7 @@
       <select class="form-control" id="genere" name="genere">
       <?php
             //includo la connessione in modo da non riscriverla più volte
-            include '../connection/connection.php';
+            include 'connection.php';
 
             $sql = "SELECT * from ruolo";
             $result = $connessione->query($sql);
@@ -81,4 +82,5 @@
 </body>
 
 </html>
+ho modificato
 
